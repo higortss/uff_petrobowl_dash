bd010c5e2a45505178e2941bafb17e51e88b3606
DuckRodgers <53906002+DuckRodgers@users.noreply.github.com>
2020-03-16 19:27:42 -0300

    Add files via upload

---
+++ javascript_petrobowl.js
@@ -0,0 +1,6 @@
+//Set the location property to create a msg box
+
+function myMsgBox() {
+	document.getElementById("play_butt").innerHTML = "Playing";
+	document.getElementById("counter_8").innerHTML = "07;59";
+}
